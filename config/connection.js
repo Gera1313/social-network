ba7a57325b5518bbf30c7f4df63d200e2f348d5e
Mark Anthony <touya1313@hotmail.com>
2023-11-03 16:35:13 -0600

    added error to connection.js

--- config/connection.js
+++ config/connection.js
@@ -4,9 +4,14 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network'
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
-    UseFindAmdModify: false,
+    UseFindAndModify: false,
 });
 
 const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+    console.log('Connected to MongoDB database');
+});
 
 module.exports = db;

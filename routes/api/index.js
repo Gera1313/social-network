5262f9e61985e7ba3215624e8fcba67b5fe03b83
Mark Anthony <touya1313@hotmail.com>
2023-11-04 03:54:09 -0600

    added another index.js file. To api to see if fixed issue

---
+++ routes/api/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Import your route files
+const userRoutes = require('./userRoutes');
+const thoughtRoutes = require('./thoughtRoutes');
+
+// Use your routes
+app.use('/api/users', userRoutes);
+app.use('/api/thoughts', thoughtRoutes);
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});

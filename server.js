01fd7f7ea6494ba8e39bfaa20f45a2bcad9c1125
Mark Anthony <touya1313@hotmail.com>
2023-11-04 05:19:43 -0600

    was able to get rid of error message, but still cannot get

--- server.js
+++ server.js
@@ -2,9 +2,14 @@ const express = require('express');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+const userRoutes = require('./routes/api/userRoutes');
+const thoughtRoutes = require('./routes/api/thoughtRoutes');
+
 const PORT = process.env.PORT || 3002;
 const app = express();
 
+app.use('/api/users', userRoutes);
+app.use('/api/thoughts', thoughtRoutes);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);

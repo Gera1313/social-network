30b47642102aaf84d7e013056b750efb9b0deb06
Mark Anthony <touya1313@hotmail.com>
2023-11-04 04:04:27 -0600

    still getting errors

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const thoughtRoutes = require('./thoughtRoutes');
+const userRoutes = require('../routes/api/userRoutes');
+const thoughtRoutes = require('../routes/api/thoughtRoutes');
 
 router.use('/users', userRoutes);
 router.use('/thoughts', thoughtRoutes);

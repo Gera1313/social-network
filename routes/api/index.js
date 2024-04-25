3e0601dffb738e95b3b4e1659c3f0534eba5d17a
Mark Anthony <touya1313@hotmail.com>
2024-04-25 17:20:35 -0600

    changes in index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,16 +1,22 @@
+// const express = require('express');
+// const app = express();
+// const PORT = process.env.PORT || 3002;
+
+// // Import your route files
+// const userRoutes = require('./userRoutes');
+// const thoughtRoutes = require('./thoughtRoutes');
+
+// // Use your routes
+// app.use('/api/users', userRoutes);
+// app.use('/api/thoughts', thoughtRoutes);
+
 const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3002;
+const router = express.Router();
 
-// Import your route files
-const userRoutes = require('./userRoutes');
 const thoughtRoutes = require('./thoughtRoutes');
+const userRoutes = require('./userRoutes');
 
-// Use your routes
-app.use('/api/users', userRoutes);
-app.use('/api/thoughts', thoughtRoutes);
+router.use('/thoughts', thoughtRoutes);
+router.use('/users', userRoutes);
 
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+module.exports = router;

f3b3e1c0b3387f092e5b2105363fd4c2eb0096b0
Mark Anthony <touya1313@hotmail.com>
2024-04-28 00:11:47 -0600

    cleaned up my server.js file

--- server.js
+++ server.js
@@ -2,18 +2,9 @@ const express = require('express');
 const db = require('./config/connection');
 const routes = require('./routes');
 
-const userRoutes = require('./routes/api/userRoutes');
-const thoughtRoutes = require('./routes/api/thoughtRoutes');
-
 const PORT = process.env.PORT || 3002;
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Welcome to the Social Network API');
-});
-
-app.use('/api/users', userRoutes);
-app.use('/api/thoughts', thoughtRoutes);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);

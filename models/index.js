7f747c4c7601362a92f0badbfff36a3739ac3dba
Mark Anthony <touya1313@hotmail.com>
2024-04-29 21:43:04 -0600

    notes

--- models/index.js
+++ models/index.js
@@ -3,3 +3,5 @@ const Thought = require('./Thought');
 const Reaction = require('./Reaction');
 
 module.exports = { User, Thought, Reaction };
+
+// Maybe come back to this. See if only User and Thought go here. 

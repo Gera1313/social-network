65087dc2914c7e736f0575871b032c82e91b4c2e
Mark Anthony <touya1313@hotmail.com>
2024-04-29 21:42:27 -0600

    index.js in models folder

--- models/index.js
+++ models/index.js
@@ -1,6 +1,5 @@
-const User = require('../models/user');
-const Thought = require('../models/thought');
+const User = require('./User');
+const Thought = require('./Thought');
+const Reaction = require('./Reaction');
 
-module.exports = { User, Thought };
-
-// review this again later
+module.exports = { User, Thought, Reaction };

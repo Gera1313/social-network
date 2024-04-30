178ea6a503bff423f797573a66bc7e9cb38b4841
Mark Anthony <touya1313@hotmail.com>
2024-04-29 21:33:24 -0600

    did userroutes.js

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,39 +1,33 @@
-const express = require('express');
-const router = express.Router();
-
-// Imports necessary controller functions (define these in userController.js)
+const router = require('express').Router();
 const {
-    getAllUsers,
-    getUserById,
-    createUser,
-    updateUser,
-    deleteUser,
-    addFriend,
-    removeFriend
-  } = require('../../controllers/userController');
-  
-  // Defines user-related routes
-  
-  // GET all users
-  router.get('/', getAllUsers);
-  
-  // GET a user by ID
-  router.get('/:userId', getUserById);
-  
-  // POST a new user
-  router.post('/', createUser);
-  
-  // PUT (update) a user by ID
-  router.put('/:userId', updateUser);
-  
-  // DELETE a user by ID
-  router.delete('/:userId', deleteUser);
-  
-  // POST to add a friend to a user's friend list
-  router.post('/:userId/friends/:friendId', addFriend);
-  
-  // DELETE to remove a friend from a user's friend list
-  router.delete('/:userId/friends/:friendId', removeFriend);
-  
-module.exports = router;
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend
+} = require('../../controllers/userController');
 
+// GET all users
+router.get('/', getAllUsers);
+
+// GET a user by ID
+router.get('/:userId', getUserById);
+
+// POST a new user
+router.post('/', createUser);
+
+// PUT (update) a user by ID
+router.put('/:userId', updateUser);
+
+// DELETE a user by ID
+router.delete('/:userId', deleteUser);
+
+// POST to add a friend to a user's friend list
+router.post('/:userId/friends/:friendId', addFriend);
+
+// DELETE to remove a friend from a user's friend list
+router.delete('/:userId/friends/:friendId', removeFriend);
+
+module.exports = router;

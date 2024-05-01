bb2f45e7cff03814d8179515644c554dadf9e03f
Mark Anthony <touya1313@hotmail.com>
2024-04-30 22:25:31 -0600

    reaction.js

--- models/reaction.js
+++ models/reaction.js
@@ -1,9 +1,18 @@
 const { Schema, Types } = require('mongoose');
 const moment = require('moment');
 
-const reactionSchema = new Schema({
-    reactionID: 
-})
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+        type: String,
+      required: true,
+      maxlength: 280,
+    }
+);
 
 
 module.exports = Reaction;

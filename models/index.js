8fed3b32c1e09bd6e85e54fe87a547f4207b2ce5
Mark Anthony <touya1313@hotmail.com>
2024-04-30 22:32:53 -0600

    edited index.js

--- models/index.js
+++ models/index.js
@@ -1,7 +1,6 @@
 const User = require('./User');
 const Thought = require('./Thought');
-const Reaction = require('./Reaction');
 
 module.exports = { User, Thought, Reaction };
 
-// Maybe come back to this. See if only User and Thought go here. 
+// DONE
--- models/reaction.js
+++ models/reaction.js
@@ -2,22 +2,32 @@ const { Schema, Types } = require('mongoose');
 const moment = require('moment');
 
 const reactionSchema = new Schema(
-    {
-        reactionId: {
-            type: Schema.Types.ObjectId,
-            default: () => new Types.ObjectId(),
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
     },
     reactionBody: {
-        type: String,
+      type: String,
       required: true,
       maxlength: 280,
     },
     username: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
-    
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => moment(timestamp).format('MMM Do, YYYY [at] hh:mm a'),
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
 );
 
-
-module.exports = Reaction;
+module.exports = reactionSchema;

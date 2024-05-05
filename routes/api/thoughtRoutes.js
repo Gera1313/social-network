01060f01e5497d6b61c2621426635ed0ebaa955d
Mark Anthony <touya1313@hotmail.com>
2024-05-05 12:58:53 -0600

    reorganized thoughtroutes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,31 +1,28 @@
-const router = require('express').Router();
+const router = require("express").Router();
 const {
   getThoughts,
-  getThoughtById,
+  getSingleThought,
   createThought,
   updateThought,
   deleteThought,
-  createReaction,
-  deleteReaction,
-} = require('../../controllers/thoughtController');
+  addReaction,
+  removeReaction,
+} = require("../../controllers/thoughtController");
 
 // /api/thoughts
-router.route('/')
-  .get(getThoughts)
-  .post(createThought);
+router.route("/").get(getThoughts).post(createThought);
 
 // /api/thoughts/:thoughtId
-router.route('/:thoughtId')
-  .get(getThoughtById)
+router
+  .route("/:thoughtId")
+  .get(getSingleThought)
   .put(updateThought)
   .delete(deleteThought);
 
 // /api/thoughts/:thoughtId/reactions
-router.route('/:thoughtId/reactions')
-  .post(createReaction);
+router.route("/:thoughtId/reactions").post(addReaction);
 
 // /api/thoughts/:thoughtId/reactions/:reactionId
-router.route('/:thoughtId/reactions/:reactionId')
-  .delete(deleteReaction);
+router.route("/:thoughtId/reactions/:reactionId").delete(removeReaction);
 
 module.exports = router;

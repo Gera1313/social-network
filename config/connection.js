cc5e018e0afa90c6754e663b2050cabbb763a842
Mark Anthony <touya1313@hotmail.com>
2024-05-08 22:56:03 -0600

    notes

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,19 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/social-network",);
+// mongoose.connect(
+//   process.env.MONGODB_URI || "mongodb://localhost/social-network",);
 
-module.exports = mongoose.connection;
+// module.exports = mongoose.connection;
 
 // DONE
+
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/socialmedia', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+module.exports = mongoose.connection;
